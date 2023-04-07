72b649bf7d76f8219378cb3c62042cdf32dd40e2
jvill171 <juanvillarreal64@yahoo.com>
2023-04-06 20:22:21 -0500

    Fixed issue where running test file overwrote working DB

--- test_models.py
+++ test_models.py
@@ -7,21 +7,21 @@ from models import db, User, DEFAULT_IMAGE_URL
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///users_test'
 app.config['SQLALCHEMY_ECHO'] = False
 
-db.drop_all()
-db.create_all()
-
 class UserModelTests(TestCase):
     '''Tests for model of Users'''
 
-    def setUp(self):
-        '''Clean up any existing users'''
-        User.query.delete()
+    # def setUp(self):
+    #     '''Clean up any existing users'''
+    #      with app.app_context():
+    #          User.query.delete()
     
     def tearDown(self):
         '''Clean up any transactions'''
-        db.session.rollback()
+        with app.app_context():
+            db.session.rollback()
 
     def test_full_name(self):
         '''Test the full_name property, ensuring it returns the "first_name last_name" value'''
-        user = User(first_name='Colt', last_name='Steele', image_url=DEFAULT_IMAGE_URL)
-        self.assertEquals(user.full_name, "Colt Steele")
+        with app.app_context():
+            user = User(first_name='Colt', last_name='Steele', image_url=DEFAULT_IMAGE_URL)
+            self.assertEquals(user.full_name, "Colt Steele")
