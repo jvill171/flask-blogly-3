3ed91dfd12da5c400173641febb2c72434f89b3c
jvill171 <juanvillarreal64@yahoo.com>
2023-04-08 16:57:56 -0500

    Added Tag & PostTag classes

--- models.py
+++ models.py
@@ -49,3 +49,21 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now())
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     
+class PostTag(db.Model):
+    '''PostTag.'''
+    
+    __tablename__='posts_tags'
+
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), primary_key=True)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), primary_key=True)
+
+
+class Tag(db.Model):
+    '''Tag.'''
+    
+    __tablename__='tags'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+
+    posts = db.relationship('Post', secondary='posts_tags', backref='tags')
